79e0a12b45fa9e46139dc355ef5024f74b46b28a
Ryan <kim1885@purdue.edu>
2019-03-26 20:42:09 -0400

    add starting project

---
+++ script.js
@@ -0,0 +1,44 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+//random color generator in hex value
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	console.log(color);
+	return color;
+}
+
+//When button is clicked generate two random color values
+//Sets the background gradient to the random colors
+function setRandomColor() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+}
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", setRandomColor);
+
+document.addEventListener('DOMContentLoaded',setRandomColor);
